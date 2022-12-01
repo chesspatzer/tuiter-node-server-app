5a30b0584425c0651b38c1b79104d1d70ff7f54b
Vishwanath Jeyaraman <vish@Vishwanaths-MacBook-Pro.local>
2022-11-30 19:17:26 -0500

    Completed database changes A9

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+
+// mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
 // app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
